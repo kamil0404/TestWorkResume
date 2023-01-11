eb7cb45f5b4254c7a26a4cffa1796640c2ac09e1
Kamil0404 <84449358+kamil0404@users.noreply.github.com>
2023-01-11 21:23:45 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const navBtnImg = document.querySelector('#nav-button-img')
 
 navButton.onclick = () => {
     if(nav.classList.toggle('open')) {
-        navBtnImg.src = '../images/social-icons/close-svgrepo-com.svg'
+        navBtnImg.src = './images/social-icons/close-svgrepo-com.svg'
     } else {
         navBtnImg.src = './images/social-icons/menu-hamburger-svgrepo-com.svg'
     }
