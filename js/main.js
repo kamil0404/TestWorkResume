5773a72448547356971fa0d6ee39388a9769b8c4
Kamil0404 <84449358+kamil0404@users.noreply.github.com>
2023-01-11 21:18:24 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('#nav')
+const navButton = document.querySelector('#nav-button')
+const navBtnImg = document.querySelector('#nav-button-img')
+
+navButton.onclick = () => {
+    if(nav.classList.toggle('open')) {
+        navBtnImg.src = '../images/social-icons/close-svgrepo-com.svg'
+    } else {
+        navBtnImg.src = './images/social-icons/menu-hamburger-svgrepo-com.svg'
+    }
+}
